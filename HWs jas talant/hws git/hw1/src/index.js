7a4422cd2e83c5f62028587793ff5f89ece5b2ec
Asem Tynyshtybai <assem.tynyshtybai@kassa24.kz>
2022-03-18 03:46:23 +0600

    filter-sort table

---
+++ HWs jas talant/hws git/hw1/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Les3 from './les2/Les3';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Les3 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
