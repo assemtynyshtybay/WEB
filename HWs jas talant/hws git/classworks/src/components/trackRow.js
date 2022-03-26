d42f2e324c4b35d73910a816d1cc5a1460993f22
Asem Tynyshtybai <asem.tynyshtybai@kassa24.kz>
2022-03-26 22:21:45 +0600

    add link to shazam

--- HWs jas talant/hws git/classworks/src/components/trackRow.js
+++ HWs jas talant/hws git/classworks/src/components/trackRow.js
@@ -11,7 +11,7 @@ function TrackRow({track}) {
             <div className="mus-title">{track.name}</div></td>
           <td>{track.artist.name}</td>
           <td>{track.playcount}</td>
-          <td><button type="button" onclick={track.url}>Shazam</button></td>
+          <td><a href={track.url} target="_blank" rel="noreferrer"><button type="button" onclick={track.url}>Shazam</button></a></td>
       </tr>
     </>
   )
